a2c49f60241e3ad497df4f3998049ebe8c110130
rahmatvisual19 <rahmatmaulanamiftah34@gmail>
2024-05-07 16:02:30 +0700

    navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,16 +12,32 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Terrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr') }}
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="border-b-2 border-transparent hover:border-purple-500 focus:border-purple-500">
+                        {{ __('Restoran') }}
+                    </x-nav-link>
+
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="border-b-2 border-transparent hover:border-purple-500 focus:border-purple-500">
+                        {{ __('Cafe') }}
+                    </x-nav-link>
+
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="border-b-2 border-transparent hover:border-purple-500 focus:border-purple-500">
+                        {{ __('Warkop') }}
+                    </x-nav-link>
+
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="border-b-2 border-transparent hover:border-purple-500 focus:border-purple-500">
+                        {{ __('Kantin') }}
+                    </x-nav-link>
+
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="border-b-2 border-transparent hover:border-purple-500 focus:border-purple-500">
+                        {{ __('Rumah Makan') }}
                     </x-nav-link>
                 </div>
-            </div>
+
 
             
 
-            <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ms-6">
+           <!-- Settings Dropdown -->
+           <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
